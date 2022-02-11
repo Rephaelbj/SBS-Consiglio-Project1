0c0cbd84d21c2f44c889f6aa75296cfed2f55dad
Rephael B. Jackson <rephael_jackson@yahoo.com>
2022-02-11 13:44:49 -0700

    Addressed comments

--- ScrumBoardSimulator/src/main/java/se/bettercode/Main.java
+++ ScrumBoardSimulator/src/main/java/se/bettercode/Main.java
@@ -6,10 +6,6 @@ import se.bettercode.scrum.Sprint;
 import se.bettercode.scrum.team.CobraTeam;
 import se.bettercode.scrum.team.Team;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
