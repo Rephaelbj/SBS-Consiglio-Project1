f54b7b8aace256e6cd4b8e900bec2c14aa49edfe
GianniConsiglio <59891516+GianniConsiglio@users.noreply.github.com>
2022-02-16 16:36:32 -0700

    starting of the reading in created strategies

---
+++ ScrumBoardSimulator/src/main/java/se/bettercode/scrum/backlog/CustomSlicedBacklog.java
@@ -0,0 +1,37 @@
+package se.bettercode.scrum.backlog;
+
+import se.bettercode.scrum.RandomStoryTitleGenerator;
+import se.bettercode.scrum.Story;
+
+import java.util.ArrayList;
+
+public class CustomSlicedBacklog extends Backlog {
+
+    private int storyCount = 10;
+    private int pointCount = 0;
+    private int pointsPerStory = 0;
+    public CustomSlicedBacklog() {
+        super("palceholder");
+         storyCount = 0;
+         pointCount = 0;
+         pointsPerStory = 0;
+
+    }
+    public void setStoryCount(int num){
+        storyCount = num;
+    }
+
+    public void setPointCount(int parseInt) {
+        pointCount = parseInt;
+    }
+    public void init(){
+        ArrayList<String> storyTitles = (new RandomStoryTitleGenerator()).generate(storyCount);
+        for(int i = 0; i < storyCount; i++){
+            addStory(new Story(3, storyTitles.get(i)));
+        }
+    }
+
+    public void setPointsPerStory(int parseInt) {
+        pointsPerStory = parseInt;
+    }
+}
--- ScrumBoardSimulator/src/main/java/se/bettercode/scrum/backlog/SelectableBacklogs.java
+++ ScrumBoardSimulator/src/main/java/se/bettercode/scrum/backlog/SelectableBacklogs.java
@@ -2,15 +2,59 @@ package se.bettercode.scrum.backlog;
 
 import se.bettercode.utils.Selectable;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class SelectableBacklogs extends Selectable<Backlog> {
 
     public SelectableBacklogs() {
+        ArrayList<Backlog> logs = loadBacklogs();
         Backlog smallBacklog = new SmallBacklog();
         Backlog wellSlicedBacklog = new WellSlicedBacklog();
         Backlog poorlySlicedBacklog = new PoorlySlicedBacklog();
+        logs.forEach((strat)->{put(strat.getName(), strat);});
         put(smallBacklog.getName(), smallBacklog);
         put(wellSlicedBacklog.getName(), wellSlicedBacklog);
         put(poorlySlicedBacklog.getName(), poorlySlicedBacklog);
     }
 
+    private ArrayList<Backlog> loadBacklogs() {
+        ArrayList<Backlog> create = new ArrayList<Backlog>();
+        try {
+            BufferedReader properties = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/StrategySave.txt")));
+            CustomSlicedBacklog item = new CustomSlicedBacklog();
+            int count = 0;
+            for (String option = ""; option != null; option = properties.readLine()) {
+                if(option.length()>=1){
+                    System.out.println(option);
+                    if(count < 4){
+                        if(count == 0){
+                            item.setName(option);
+                        }else if(count == 1){
+                            item.setPointCount(Integer.parseInt(option.replace("\n", "")));
+                        }else if(count == 2){
+                            item.setStoryCount(Integer.parseInt(option.replace("\n", "")));
+                        }else if(count == 3){
+                            item.setPointsPerStory(Integer.parseInt(option.replace("\n", "")));
+                            item.init();
+                            create.add(item);
+                        }
+                    }else{
+                        count = 0;
+                        item = new CustomSlicedBacklog();
+                        item.setName(option);
+                    }
+                    count++;
+                }else if(count == 3){
+                    item.init();
+                    create.add(item);
+                    count++;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return create;
+    }
 }
