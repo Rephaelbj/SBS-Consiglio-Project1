2e7f9c5c496baf46f71410f5d4182fab69547c7e
Rephael B. Jackson <rephael_jackson@yahoo.com>
2022-02-23 22:24:24 -0700

    Added loading and saving teams

--- ScrumBoardSimulator/src/main/java/se/bettercode/scrum/gui/NewTeamWindow.java
+++ ScrumBoardSimulator/src/main/java/se/bettercode/scrum/gui/NewTeamWindow.java
@@ -6,18 +6,24 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import se.bettercode.scrum.team.SelectableTeams;
+import se.bettercode.scrum.team.Team;
+import se.bettercode.scrum.team.TeamImpl;
 
 public class NewTeamWindow extends GridPane {
 
     public Stage currentStage;
     private SelectableTeams teams;
 
-    public NewTeamWindow(SelectableTeams team)
-    {
+    public NewTeamWindow(SelectableTeams team) {
         teams = team;
         Label titleLabel = new Label("New Team");
+        Label errorLabel = new Label("Invalid input");
+        errorLabel.setTextFill(Color.RED);
+        errorLabel.setVisible(false);
 
         TextField nameField = new TextField();
         TextField velocityField = new TextField();
@@ -26,6 +32,25 @@ public class NewTeamWindow extends GridPane {
         Label velocityLabel = new Label("Velocity: ");
 
         Button createTeam = new Button("Create Team");
+        createTeam.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    int velocity = Integer.parseInt(velocityField.getText());
+
+                    if (nameField.getText() != "") {
+                        Team team = new TeamImpl(nameField.getText(), velocity);
+                        teams.addTeam(team);
+                        currentStage.fireEvent(new WindowEvent(currentStage, WindowEvent.WINDOW_CLOSE_REQUEST));
+                    } else {
+                        errorLabel.setVisible(true);
+                    }
+
+                } catch (Exception e) {
+                    errorLabel.setVisible(true);
+                }
+            }
+        });
         Button cancel = new Button("Cancel");
 
         cancel.setOnAction(new EventHandler<ActionEvent>() {
@@ -35,20 +60,21 @@ public class NewTeamWindow extends GridPane {
             }
         });
 
-        this.add(titleLabel, 0 ,0, 2,1);
+        this.add(titleLabel, 0, 0, 2, 1);
+        this.add(errorLabel, 1, 0, 2, 1);
 
-        this.add(nameLabel, 0 , 1, 1, 1);
+
+        this.add(nameLabel, 0, 1, 1, 1);
         this.add(nameField, 1, 1, 1, 1);
 
-        this.add(velocityLabel, 0,2,1,1);
-        this.add(velocityField, 1,2,1,1);
+        this.add(velocityLabel, 0, 2, 1, 1);
+        this.add(velocityField, 1, 2, 1, 1);
 
-        this.add(createTeam,0, 3, 1,1);
-        this.add(cancel, 1, 3,1,1);
+        this.add(createTeam, 0, 3, 1, 1);
+        this.add(cancel, 1, 3, 1, 1);
     }
 
-    public void setStage(Stage newStage)
-    {
+    public void setStage(Stage newStage) {
         this.currentStage = newStage;
     }
 }
--- ScrumBoardSimulator/src/main/java/se/bettercode/scrum/team/SelectableTeams.java
+++ ScrumBoardSimulator/src/main/java/se/bettercode/scrum/team/SelectableTeams.java
@@ -2,13 +2,87 @@ package se.bettercode.scrum.team;
 
 import se.bettercode.utils.Selectable;
 
+import java.io.*;
+
 public class SelectableTeams extends Selectable<Team> {
 
-    public SelectableTeams() {
-        Team cobras = new CobraTeam();
-        Team smurfs = new SmurfTeam();
-        put(cobras.getName(), cobras);
-        put(smurfs.getName(), smurfs);
+    public SelectableTeams() throws FileNotFoundException {
+        readTeams();
+    }
+
+
+    public void readTeams() throws FileNotFoundException {
+        // Check if Folder exist
+        String path = System.getProperty("user.home") + "/Desktop/SBS Program/Teams.txt";
+        String folderPath = System.getProperty("user.home") + "/Desktop/SBS Program";
+        File file = new File(path);
+        File file1 = new File(folderPath);
+        try {
+            if (file.exists()) {
+                String input = "";
+                FileInputStream fi = new FileInputStream(file);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(fi));
+                while((input = reader.readLine()) != null)
+                {
+                    String[] values = input.split("\\|");
+                    Team newTeam = new TeamImpl(values[0], Integer.parseInt((values[1])));
+                    put(newTeam.getName(), newTeam);
+                }
+            } else {
+                if(!file1.exists())
+                {
+                    file1.mkdirs();
+                }
+                FileOutputStream fs = new FileOutputStream(file);
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fs));
+                writer.write("Cobras|23\n");
+                writer.write("The Smurfs|15\n");
+
+                Team cobras = new TeamImpl("Cobras", 23);
+                Team smurfs = new TeamImpl("The Smurfs", 15);
+
+                put(cobras.getName(), cobras);
+                put(smurfs.getName(), smurfs);
+                writer.close();
+            }
+        }
+        catch(Exception e)
+        {
+                System.out.println(e.toString());
+        }
+    }
+
+
+
+    public void addTeam(Team team) throws IOException
+    {
+        put(team.getName(), team);
+        String path = System.getProperty("user.home") + "/Desktop/SBS Program/Teams.txt";
+        String folderPath = System.getProperty("user.home") + "/Desktop/SBS Program";
+        File file = new File(path);
+        File file1 = new File(folderPath);
+        try {
+            if (file.exists()) {
+                FileOutputStream fs = new FileOutputStream(file,true);
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fs));
+                writer.write(team.getName()+ "|" + team.velocityProperty().get()+"\n");
+                writer.close();
+            } else {
+                if(!file1.exists())
+                {
+                    file1.mkdirs();
+                }
+                FileOutputStream fs = new FileOutputStream(file);
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fs));
+                writer.write(team.getName()+ "|" + team.velocityProperty().get()+"\n");
+                writer.close();
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.toString());
+        }
+
     }
 
 }
