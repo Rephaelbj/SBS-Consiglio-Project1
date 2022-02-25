f2dae5b36a7f7f91a7f81f8cc10ae610edd59146
GianniConsiglio <59891516+GianniConsiglio@users.noreply.github.com>
2022-02-25 11:37:19 -0700

    Task 77 completed the settings menu. Credit goes to Rephael because I wanted to make sure the popups looked the same as the others so most of the code is set up the same for sake of simularity of menus

--- ScrumBoardSimulator/src/main/java/se/bettercode/scrum/ScrumGameApplication.java
+++ ScrumBoardSimulator/src/main/java/se/bettercode/scrum/ScrumGameApplication.java
@@ -138,6 +138,20 @@ public class ScrumGameApplication extends Application {
         MenuItem fItem3 = new MenuItem("Import from Taiga");
         MenuItem fItem4 = new MenuItem("Export to Taiga");
         MenuItem fItem5 = new MenuItem("Settings");
+        fItem5.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Stage stage = new Stage();
+                SettingsMenu settings = new SettingsMenu(stage);
+                settings.setAlignment(Pos.CENTER);
+                settings.setHgap(10);
+                settings.setVgap(10);
+                Scene scene = new Scene(settings, 400, 300);
+                stage.setScene(scene);
+                stage.setTitle("Settings");
+                stage.show();
+            }
+        });
         MenuItem fItem6 = new MenuItem("Exit");
         fileMenu.getItems().addAll(fItem1,fItem2,fItem3,fItem4,fItem5,fItem6);
 
