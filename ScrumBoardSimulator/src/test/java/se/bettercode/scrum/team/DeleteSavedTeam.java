9dd5d340bfb166959bb2ba20c57493f1faa71df7
David Bieganski <david.biegansk@asu.edu>
2022-03-03 17:09:03 -0700

    TG-107 #done Completed unit test for deleting team from save file
    Co-authored-by: David Bieganski <david.biegansk@asu.edu>
    Co-authored-by: Bhavana Priya Kanumuri <bhavanapriyakanumuri@gmail.com>

--- ScrumBoardSimulator/src/test/java/se/bettercode/scrum/team/DeleteSavedTeam.java
+++ ScrumBoardSimulator/src/test/java/se/bettercode/scrum/team/DeleteSavedTeam.java
@@ -2,9 +2,12 @@ package se.bettercode.scrum.team;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import org.junit.BeforeClass;
@@ -25,34 +28,41 @@ public class DeleteSavedTeam {
 		String path = System.getProperty("user.home") + "/Desktop/SBS Program/Test.txt";
         String folderPath = System.getProperty("user.home") + "/Desktop/SBS Program";
         File file = new File(path);
-        File file1 = new File(folderPath);
         try {
-            if (file.exists()) {
-                FileOutputStream fs = new FileOutputStream(file,true);
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fs));
-                writer.write(tpb.getName()+ "|" + tpb.velocityProperty().get() + "|" + tpb.maturityProperty().getValue() + "\n");
-                writer.write(tts.getName()+ "|" + tts.velocityProperty().get() + "|" + tts.maturityProperty().getValue() + "\n");
-                writer.write(koth.getName()+ "|" + koth.velocityProperty().get() + "|" + koth.maturityProperty().getValue() + "\n");
-                writer.close();
-            } else {
-                if(!file1.exists())
-                {
-                    file1.mkdirs();
-                }
-                FileOutputStream fs = new FileOutputStream(file);
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fs));
-                writer.write(tpb.getName()+ "|" + tpb.velocityProperty().get() + "|" + tpb.maturityProperty().getValue() + "\n");
-                writer.write(tts.getName()+ "|" + tts.velocityProperty().get() + "|" + tts.maturityProperty().getValue() + "\n");
-                writer.write(koth.getName()+ "|" + koth.velocityProperty().get() + "|" + koth.maturityProperty().getValue() + "\n");
-                writer.close();
-            }
+            if (file.exists()) file.delete();
+            FileOutputStream fs = new FileOutputStream(file,true);
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fs));
+            writer.write(tpb.getName()+ "|" + tpb.velocityProperty().get() + "|" + tpb.maturityProperty().getValue() + "\n");
+            writer.write(tts.getName()+ "|" + tts.velocityProperty().get() + "|" + tts.maturityProperty().getValue() + "\n");
+            writer.write(koth.getName()+ "|" + koth.velocityProperty().get() + "|" + koth.maturityProperty().getValue() + "\n");
+            writer.close();
         }
         catch(Exception e) {}
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		teams.deleteTeam("The Three Stooges");
+		
+		String expectedFileContents = "Trailor Park Boys|23|ExpertKing of the Hill|35|Beginner";
+		
+		String actualFileContents = "";
+		String path = System.getProperty("user.home") + "/Desktop/SBS Program/Test.txt";
+        File file = new File(path);
+        try {
+            if (file.exists()) {
+                String input = "";
+                FileInputStream fi = new FileInputStream(file);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(fi));
+                while((input = reader.readLine()) != null)
+                	actualFileContents += input;
+            } else fail("File not found!");
+        }
+        catch(Exception e) {}
+        actualFileContents = actualFileContents.trim();
+		
+        System.out.println(actualFileContents);
+		assertEquals(expectedFileContents, actualFileContents);
 	}
 
 }
